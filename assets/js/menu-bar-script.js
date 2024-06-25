d815b7c501a0aae0fac538737c7d938e1c16e9fc
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-06-25 14:53:13 +0200

    update on side-menu to better fit design-draft. added functionality to get the current-menu-point and highlight active point

--- assets/js/menu-bar-script.js
+++ assets/js/menu-bar-script.js
@@ -2,7 +2,7 @@ async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
         const element = includeElements[i];
-        const file = element.getAttribute("w3-include-html"); 
+        const file = element.getAttribute("w3-include-html");
         let resp = await fetch(file);
         if (resp.ok) {
             element.innerHTML = await resp.text();
@@ -10,4 +10,39 @@ async function includeHTML() {
             element.innerHTML = 'Page not found';
         }
     }
+    updateMenu();
+}
+
+/**
+ * fetchs the currently open html-file-name and removes .html.
+ * This function is used to handle the navigation-design.
+ * @returns string with current file.html
+ */
+function getCurrentFileName() {
+    const path = window.location.pathname;
+    let fileName = path.substring(path.lastIndexOf('/') + 1);
+    return fileName.replace('.html', '');
+}
+
+/**
+ * 
+ */
+function updateMenu() {
+    const currentlyOpen = getCurrentFileName();
+    if (regularMenuPoint(currentlyOpen)) {
+        const menuitemDesk = document.getElementById(`${currentlyOpen}-desk-link`);
+        const menuitemMobile = document.getElementById(`${currentlyOpen}-mobile-link`);
+        menuitemDesk.href = '#'; menuitemMobile.href = '#';
+        menuitemDesk.classList.add('active');
+        menuitemMobile.classList.add('active');
+    } else {
+        const menuitem = document.getElementById(`${currentlyOpen}-link`);
+        menuitem.href = '#';
+        menuitem.classList.add('active');
+    }
+}
+
+function regularMenuPoint(currentPoint) {
+    const specialPages = ["privacy", "legal_notice", "help"];
+    return !specialPages.includes(currentPoint);
 }
