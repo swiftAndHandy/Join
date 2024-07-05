bde1069890f7ed0763ae593c596bfdcde8d35a9a
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-07-05 14:44:31 +0200

    moved user-avatar creation to menu-bar.js

--- assets/js/menu-bar-script.js
+++ assets/js/menu-bar-script.js
@@ -94,3 +94,12 @@ function forceLogout(currentFile) {
         logout();
     }
 }
+
+/**
+ * Updates Content of Menu-Bar-Avatar
+ * @param {string} content - Initials of logged in Account or - if not logged in 'G'.
+ */
+function avatarHtml(content) {
+    document.getElementById('my-avatar-desktop').innerHTML = content;
+    document.getElementById('my-avatar-mobile').innerHTML = content;
+}
