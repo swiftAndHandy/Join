47302653719445147ed37fdfe4ab11c95b15e167
Andre Veltens <158059006+swiftAndHandy@users.noreply.github.com>
2024-07-10 13:33:01 +0200

    smoother delete of contacts

---
+++ assets/js/contacts-html.js
@@ -0,0 +1,73 @@
+/**
+ * Creates and inserts HTML for a contact list item.
+ * 
+ * @param {number} index - The index of the contact.
+ * @param {string} name - The name of the contact.
+ * @param {string} email - The email of the contact.
+ * @param {string} color - The color associated with the contact.
+ */
+function createContactListHTML(index, name, email, color) {
+    document.getElementById('contact-list').innerHTML += `
+    <li id="contact-item-${index}" class="contact-item" onclick="openContactDetails('${index}')">
+            <div class="contact-image--small" style="background-color:${color};">${showCapitaliseFirstLetters(name)}</div>
+            <div class="contact-details">
+                <p class="contact-name">${name}</p>
+                <p class="contact-email">${email}</p>
+            </div>
+        </li>
+    `;
+}
+
+
+/**
+ * Creates and inserts HTML for a contact list letter header.
+ * 
+ * @param {string} letter - The letter to display as a header.
+ */
+function createContactLetterHTML(letter) {
+    document.getElementById('contact-list').innerHTML += `
+        <li class="letter">${letter}</li>
+        <div class="underline"></div>
+    `;
+}
+
+
+/**
+ * Creates and inserts HTML for displaying the contact details.
+ * 
+ * @param {string} name - The name of the contact.
+ * @param {string} email - The email of the contact.
+ * @param {string} telefon - The phone number of the contact.
+ * @param {string} color - The color associated with the contact.
+ * @param {number} i - The index of the contact.
+ */
+function contactDetailsHTML(name, email, telefon, color, i) {
+    let contactInfoContainer = document.getElementById('contact-info-container');
+    contactInfoContainer.style.display = 'block';
+    contactInfoContainer.innerHTML = `
+        <div class="image-name-container">
+            <div class="contact-image--large" style="background-color:${color};">${showCapitaliseFirstLetters(name)}</div>
+            <div class="name-buttons-container">
+                <h2 class="contact-name--large">${name}</h2>
+                <div class="edit-delete-buttons-container">
+                    <div id="edit-button" class="edit-button" onclick="openEditContactPage('${i}')">
+                        <img src="./assets/img/icons/edit.svg" alt="edit">edit
+                    </div>
+                    <div id="delete-button" class="delete-button" onclick="deleteContact('contacts/${i}', '${contactToEditId}')">
+                        <img src="./assets/img/icons/delete.svg" alt="delete">delete
+                    </div>
+                </div>
+            </div>
+        </div>
+        <span class="span-contact-info">Contact information</span>
+        <div class="email-phone-container">
+            <span class="span-email-phone">Email</span>
+            <a id="current-contact-email" href="mailto:${email}" class="contact-email">${email}</a>
+            <span class="span-email-phone">Phone</span>
+            <a id="current-contact-phone" href="tel:${telefon}" class="tel-number">${telefon}</a>
+        </div>
+    `;
+    document.getElementById('edit-button').style.display = 'flex';
+    document.getElementById('delete-button').style.display = 'flex';
+}
+
--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -111,40 +111,6 @@ async function putContactsToList() {
 
 
 /**
- * Creates and inserts HTML for a contact list item.
- * 
- * @param {number} index - The index of the contact.
- * @param {string} name - The name of the contact.
- * @param {string} email - The email of the contact.
- * @param {string} color - The color associated with the contact.
- */
-function createContactListHTML(index, name, email, color) {
-    document.getElementById('contact-list').innerHTML += `
-    <li id="contact-item-${index}" class="contact-item" onclick="openContactDetails('${index}')">
-            <div class="contact-image--small" style="background-color:${color};">${showCapitaliseFirstLetters(name)}</div>
-            <div class="contact-details">
-                <p class="contact-name">${name}</p>
-                <p class="contact-email">${email}</p>
-            </div>
-        </li>
-    `;
-}
-
-
-/**
- * Creates and inserts HTML for a contact list letter header.
- * 
- * @param {string} letter - The letter to display as a header.
- */
-function createContactLetterHTML(letter) {
-    document.getElementById('contact-list').innerHTML += `
-        <li class="letter">${letter}</li>
-        <div class="underline"></div>
-    `
-}
-
-
-/**
  * Returns the capitalized first letters of the words in the input string.
  * 
  * @param {string} input - The input string.
@@ -213,47 +179,6 @@ function changeContactItemColor(userId) {
 }
 
 
-
-/**
- * Creates and inserts HTML for displaying the contact details.
- * 
- * @param {string} name - The name of the contact.
- * @param {string} email - The email of the contact.
- * @param {string} telefon - The phone number of the contact.
- * @param {string} color - The color associated with the contact.
- * @param {number} i - The index of the contact.
- */
-function contactDetailsHTML(name, email, telefon, color, i) {
-    let contactInfoContainer = document.getElementById('contact-info-container');
-    contactInfoContainer.style.display = 'block';
-    contactInfoContainer.innerHTML = `
-        <div class="image-name-container">
-            <div class="contact-image--large" style="background-color:${color};">${showCapitaliseFirstLetters(name)}</div>
-            <div class="name-buttons-container">
-                <h2 class="contact-name--large">${name}</h2>
-                <div class="edit-delete-buttons-container">
-                    <div id="edit-button" class="edit-button" onclick="openEditContactPage('${i}')">
-                        <img src="./assets/img/icons/edit.svg" alt="edit">edit
-                    </div>
-                    <div id="delete-button" class="delete-button" onclick="deleteContact('/contacts/${i}')">
-                        <img src="./assets/img/icons/delete.svg" alt="delete">delete
-                    </div>
-                </div>
-            </div>
-        </div>
-        <span class="span-contact-info">Contact information</span>
-        <div class="email-phone-container">
-            <span class="span-email-phone">Email</span>
-            <a id="current-contact-email" href="mailto:${email}" class="contact-email">${email}</a>
-            <span class="span-email-phone">Phone</span>
-            <a id="current-contact-phone" href="tel:${telefon}" class="tel-number">${telefon}</a>
-        </div>
-    `;
-    document.getElementById('edit-button').style.display = 'flex';
-    document.getElementById('delete-button').style.display = 'flex';
-}
-
-
 /**
  * Creates a new contact by sending the data to the server and updates the contact list.
  * 
@@ -267,7 +192,7 @@ async function createContact(event) {
 
     await postData({
         'email': emailInput,
-        'name': nameInput,
+        'name': capitaliseFirstLetters(nameInput),
         'phone': phoneInput,
         'color': applyRandomColor()
     }, 'contacts');
@@ -319,23 +244,54 @@ function closeContactModal() {
 
 
 /**
- * Deletes a contact from the server and updates the contact list.
+ * Deletes a contact from the server and triggers an update of the contact list.
  * 
  * @param {string} path - The path to the contact data.
  */
-async function deleteContact(path = "") {
-    let response = await fetch(BASE_URL + path + ".json", {
-        method: "DELETE",
-    });
-    await putContactsToList();
-    document.getElementById('contact-info-container').style.display = 'none';
+async function deleteContact(path = "", contactId) {
+    // await deleteData(path);
+    const container = document.getElementById('contact-info-container');
+    container.innerHTML = ''; 
+    container.nextElementSibling.nextElementSibling.style = '';
+    container.nextElementSibling.style = '';
+    container.style = '';
+    
+    removeContactFromInterface(contactId);
+    document.getElementById('modalBodyContent').innerHTML = '';
+    mobileDeleteRules();
+}
 
+
+/**
+ * Chances some styles to display the correct screen, when contacts is opened on a mobile device
+ */
+function mobileDeleteRules() {
     executeOnMaxWidth(820, async () => {
         document.getElementById('contact-list-container').style.display = 'block';
         document.getElementById('contact-window').style.display = 'none';
         document.getElementById('more-vert-button').style.display = 'none';
     });
-    return await response.json();
+}
+
+/**
+ * If the deleted contact is the last contact of a letter, the letter and the underline must 
+ * be deleted. So this function uses the current contacts position on contact-list and checks 
+ * the previous and next item. If the next one is another letter, and the previous one is an underline
+ * it IS the last item. If there is no next sibling, simulate it has the value of 'letter' to prevent a crash.
+ * So the previous to Element Siblings needs to be deleted.
+ * The item itself needs to be deleted in each case.
+ * @param {string} - the ID of the Contact that needs to be checked
+ */
+function removeContactFromInterface(contactId) {
+    const item = document.getElementById(`contact-item-${contactId}`);
+    let previousElement = item.previousElementSibling.classList.value;
+    let nextElement = item.nextElementSibling ? item.nextElementSibling.classList.value : 'letter';
+
+    if (previousElement == 'underline' && nextElement == 'letter') {
+        item.previousElementSibling.remove();
+        item.previousElementSibling.remove();
+    }
+    item.remove();
 }
 
 /**
